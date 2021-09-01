64d8a7afc376798db31ceb8e830afff1eb9f784f
Allaire-Drolet <6188793@etu.cegep-lanaudiere.qc.ca>
2021-09-01 13:38:01 -0400

    Création du projet java (premier programme Java à vie)

---
+++ Atelier1/src/App.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
