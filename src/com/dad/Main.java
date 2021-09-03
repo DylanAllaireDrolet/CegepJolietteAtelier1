10cab1475fae43c092c147f0e8ae254e73f40722
6188793 <dylan.allaire.drolet.13@gmail.com>
2021-09-03 17:01:21 -0400

    Update

--- src/com/dad/Main.java
+++ src/com/dad/Main.java
@@ -7,29 +7,29 @@ import java.nio.charset.StandardCharsets;
 
 
 public class Main {
+ // Code in english, User Interface in French
 
+    public static void main(String[] args) throws  FileNotFoundException, IOException{
 
-    public static void main(String[] args)  {
-        try {
-            RandomAccessFile file = new RandomAccessFile("text.txt", "r");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        //Variables
+        RandomAccessFile file = new RandomAccessFile("text.txt", "r");
 
+        long offset = 0; // Default value
+        long length = file.length();
 
 
 
         for (int i = 0; i < args.length; i++) {
 
-            String a = args[i];
-            char[] characters=a.toCharArray();
+            String a = args[i]; // Reads each input (a = args[i]-> allo)
+
             System.out.println(args[i]);
-            for (int z =0; z < characters.length;z++) {
-                System.out.println((int) characters[z]); // Comment écrire les données en ascii
-            }
+
             // o (position) l (largeur) dans la fonction print data o = offset et l pour length
 
         }
+
+            printData(file,offset,length);
 
         // TODO: Gestion des arguments.
         // TODO: Gestion du fichier.
@@ -37,7 +37,16 @@ public class Main {
 
     public static void printData(RandomAccessFile file, long offset, long length) throws IOException {
         // TODO: Affichage des données.
+        file.seek(offset);
+        String a = file.readLine();
+        char[] userInput=a.toCharArray();
+        for (int i = 0; i < (int)length; i++) {
+            userInput[i] = file.readChar();
+        }
 
+        for (int z = 0; z < length;z++) {
+            System.out.printf("%d ",(int) userInput[z]);
+        }
     }
 
     public static void printUsage() {
@@ -51,6 +60,8 @@ public class Main {
     public static void printFormat() {
         // TODO: Affichage du format d'exécutable.
     }
+
+
 
 }
 
